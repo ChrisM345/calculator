f033c82999b47b3019b5f3d18b183a7f9a57d07b
ChrisM345 <chrism.12277@gmail.com>
2024-02-16 10:29:19 -0600

    Corrected logic for operators and added -, *, /

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,7 @@
 let prevVal = '';
 let currVal = '';
+let op = false;
+let prevOperator = '';
 const display = document.querySelector('#result');
 display.textContent = prevVal;
 
@@ -9,22 +11,44 @@ function operate(firstVal, secondVal, operator){
     switch(operator) {
         case '+':
             prevVal = firstVal + secondVal;
-            currVal = '';
-            display.textContent = prevVal;
+            break;
+        case 'X':
+            prevVal = firstVal * secondVal;
+            break;
+        case '/':
+            prevVal = firstVal / secondVal;
+            break;
+        case '-':
+            prevVal = firstVal - secondVal;
+            break;
     }
+    currVal = '';
+    display.textContent = prevVal;
 }
 
 const numberButtons = document.querySelectorAll('.num')
 numberButtons.forEach(ele => {
     ele.addEventListener('click', (e) => {
         currVal += ele.textContent;
-        console.log(prevVal);
-        console.log(currVal)
+        //console.log(prevVal);
+        //console.log(currVal)
         display.textContent = currVal;
     })
 })
 
 const operatorButtons = document.querySelectorAll('.operator');
 operatorButtons.forEach(ele => {
-    ele.addEventListener('click', () => operate(Number(prevVal), Number(currVal), ele.textContent))
+    ele.addEventListener('click', () => {
+        if (op == true) {
+            operate(Number(prevVal), Number(currVal), prevOperator)
+            prevOperator = ele.textContent;
+        }
+        else{
+            op = true;
+            prevOperator = ele.textContent;
+            prevVal = currVal;
+            display.textContent = prevVal;
+            currVal = '';
+        }
+    })
 })
