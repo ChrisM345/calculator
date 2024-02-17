be0ed9f0ceb34294808c47f1da0daff27126ba3b
ChrisM345 <chrism.12277@gmail.com>
2024-02-17 10:58:55 -0600

    Added rounding and overflow checks

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,16 @@
+const DISPLAY_MAX = 12;
+const NUMBER_MAX = 999999999999
 let prevVal = '';
 let currVal = '';
 let op = false;
 let equals = false;
 let prevOperator = '';
 const display = document.querySelector('#result');
+const prevDisplay = document.querySelector('#prev-result')
 display.textContent = prevVal;
 
+//Logic for basic operators
 function operate(firstVal, secondVal, operator){
-    console.log(firstVal);
-    console.log(secondVal);
 
     //If the secondVal is an empty string that means there was an operator immediately after =
     //We do not want to change the previous Value in this case.
@@ -22,54 +24,68 @@ function operate(firstVal, secondVal, operator){
 
     switch(operator) {
         case '+':
-            prevVal = firstVal + secondVal;
+            prevVal = (firstVal + secondVal).toFixed(3);
             break;
         case 'X':
-            prevVal = firstVal * secondVal;
+            prevVal = (firstVal * secondVal).toFixed(3);
             break;
         case '/':
-            prevVal = firstVal / secondVal;
+            prevVal = (firstVal / secondVal).toFixed(3);
             break;
         case '-':
-            prevVal = firstVal - secondVal;
+            prevVal = (firstVal - secondVal).toFixed(3);
             break;
     }
     currVal = '';
-    display.textContent = prevVal;
+    if( prevVal > NUMBER_MAX) prevVal = NUMBER_MAX;
+    display.textContent = (Number(prevVal)).toString();
 }
 
-function special(exp){
+function testOverflow(number){
+    if (number.toString().length > DISPLAY_MAX) number = Math.round(number)
+    if (number > NUMBER_MAX) number = NUMBER_MAX;
+    return (Number(number)).toString();
+}
 
+//Special options with if/else statements to see if one was pressed immediately after equals.
+function special(exp){
     switch(exp) {
         case '1/x':
             if (currVal === ''){
-                prevVal = 1/prevVal;
+                prevVal = (1/prevVal).toFixed(3);
+                prevVal = testOverflow(prevVal)
                 display.textContent = prevVal;
             }else{
-                currVal = 1/currVal;
+                currVal = (1/currVal).toFixed(3);
+                currVal = testOverflow(currVal);
                 display.textContent = currVal;
             }
             break;
         case 'x^2':
             if (currVal === ''){
                 prevVal = prevVal ** 2;
+                prevVal = testOverflow(prevVal)
                 display.textContent = prevVal;
             }else{
                 currVal = currVal ** 2;
+                currVal = testOverflow(currVal);
                 display.textContent = currVal;
             }
             break;
         case 'sqrt(x)':
             if (currVal === ''){
-                prevVal = Math.sqrt(prevVal);
+                prevVal = (Math.sqrt(prevVal)).toFixed(3);
+                prevVal = testOverflow(prevVal)
                 display.textContent = prevVal;
             }else{
-                currVal = Math.sqrt(currVal);
+                currVal = (Math.sqrt(currVal)).toFixed(3);
+                currVal = testOverflow(currVal);
                 display.textContent = currVal;
             }
             break;
         case '%':
-            currVal = (currVal / 100) * (prevVal);
+            currVal = ((currVal / 100) * (prevVal)).toFixed(3);
+            currVal = testOverflow(currVal);
             display.textContent = currVal;
             break;
         case '+/-':
@@ -81,14 +97,11 @@ function special(exp){
                 display.textContent = currVal;
             }
             break;
-
     }
-    //display.textContent = currVal;
 }
 
+//Delete options
 function del(delOperator){
-    console.log(delOperator)
-    console.log(currVal);
     switch(delOperator){
         case 'CE':
             currVal = '';
@@ -113,19 +126,17 @@ function del(delOperator){
     }
 }
 
+//Event listener for number buttons
 const numberButtons = document.querySelectorAll('.num')
 numberButtons.forEach(ele => {
     ele.addEventListener('click', (e) => {
-        if (equals == true){
-            //op = false;
-        }
-        currVal += ele.textContent;
-        //console.log(prevVal);
-        //console.log(currVal)
+        //Do not allow numbers larger than the display can fit.
+        if(currVal.length < 12) currVal += ele.textContent;
         display.textContent = currVal;
     })
 })
 
+//Event listener for operators
 const operatorButtons = document.querySelectorAll('.operator');
 operatorButtons.forEach(ele => {
     ele.addEventListener('click', () => {
@@ -144,12 +155,14 @@ operatorButtons.forEach(ele => {
     })
 })
 
+//Event listener for delete options
 const deleteButtons = document.querySelectorAll('.delete');
 deleteButtons.forEach(ele => {
     ele.addEventListener('click', () =>
     del(ele.textContent))
 })
 
+//Event listener for special options
 const specialButtons = document.querySelectorAll('.special');
 specialButtons.forEach(ele => {
     ele.addEventListener('click', () =>
